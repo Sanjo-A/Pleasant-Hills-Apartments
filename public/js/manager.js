d7ce546b1721605c0ff27d40adf8e1e2617a73be
Sanjo-A <Sanjo.a@protonmail.com>
2020-02-22 14:40:46 -0600

    hide contents of management tools

--- public/js/manager.js
+++ public/js/manager.js
@@ -2,6 +2,9 @@ window.onload = function(){
     var listRow = document.getElementById("editListingRow");
     var custRow = document.getElementById("editCustomerRow");
     var techRow = document.getElementById("editTechnicianRow");
+    var people = document.getElementById("manPeopleTxt");
+    var peopleContent = document.getElementById("peopleContent");
+
     listRow.addEventListener("click", function(event){
         window.location.href = "editListing";
     });
@@ -11,4 +14,13 @@ window.onload = function(){
     techRow.addEventListener("click", function(event){
         window.location.href = "editTechnician";
     });
+    people.addEventListener("click", function(evnet){
+        if(peopleContent.style.display == "none"){
+            peopleContent.style.display = "block";
+        }
+        else{
+            peopleContent.style.display = "none"
+        }
+    });
+
 }
