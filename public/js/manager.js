a4b7ea84239bebe12709a01fc4cb0894deadaa34
Sanjo-A <Sanjo.a@protonmail.com>
2020-02-22 15:05:33 -0600

    hide management contents

--- public/js/manager.js
+++ public/js/manager.js
@@ -4,6 +4,14 @@ window.onload = function(){
     var techRow = document.getElementById("editTechnicianRow");
     var people = document.getElementById("manPeopleTxt");
     var peopleContent = document.getElementById("peopleContent");
+    var customer = document.getElementById("manCustomerTxt");
+    var customerContent = document.getElementById('custContent')
+    var technician = document.getElementById("manTechTxt");
+    var technicianContent = document.getElementById("techContent");
+    var amenities = document.getElementById("amenTxt");
+    var amenitiesContent = document.getElementById("amenContent");
+    var listings = document.getElementById("listingTxt");
+    var listingContent = document.getElementById("listingContent");
 
     listRow.addEventListener("click", function(event){
         window.location.href = "editListing";
@@ -22,5 +30,38 @@ window.onload = function(){
             peopleContent.style.display = "none"
         }
     });
+    customer.addEventListener("click", function(){
+        if(customerContent.style.display == "none"){
+            customerContent.style.display = "block";
+        }
+        else{
+            customerContent.style.display = "none"
+        }
+    });
+    technician.addEventListener("click", function(){
+        if(technicianContent.style.display == "none"){
+            technicianContent.style.display = "block";
+        }
+        else{
+            technicianContent.style.display = "none"
+        }
+    });
+    amenities.addEventListener("click", function(){
+        if(amenitiesContent.style.display == "none"){
+            amenitiesContent.style.display = "block";
+        }
+        else{
+            amenitiesContent.style.display = "none"
+        }
+    });
+    listings.addEventListener("click", function(){
+        if(listingContent.style.display == "none"){
+            listingContent.style.display = "block";
+        }
+        else{
+            listingContent.style.display = "none"
+        }
+    });
+
 
 }
