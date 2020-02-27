28359061e137a4f82336cb33949c23d39e875ef5
Sanjo-A <Sanjo.a@protonmail.com>
2020-02-27 09:52:51 -0600

    work on resizing cards

--- public/js/apartment.js
+++ public/js/apartment.js
@@ -3,4 +3,21 @@ window.onload = function(){
     listingCard.addEventListener("click",function(event){
         window.location.href = "/apartment-details";
     });
+
+    var width = document.documentElement.clientWidth;
+    var bodyCard = document.getElementById("bodyCard");
+    var imgCard = document.getElementById("imgCard");
+    if(width < 1000){
+        imgCard.classList.remove("col-md-6");
+        imgCard.classList.add("row-*-*");
+        bodyCard.classList.remove("col-md-6");
+        bodyCard.classList.add("row");       
+    }
+    else{
+        imgCard.classList.add("col-md-6");
+        imgCard.classList.remove("row");
+        bodyCard.classList.add("col-md-6");
+        bodyCard.classList.remove("row");     
+    }
 }
+
