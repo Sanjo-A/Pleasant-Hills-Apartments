f355392721586f033eaead760e51fc39350e0e8b
Sanjo-A <Sanjo.a@protonmail.com>
2020-02-27 10:17:55 -0600

    cards resize properly

--- public/js/apartment.js
+++ public/js/apartment.js
@@ -3,21 +3,28 @@ window.onload = function(){
     listingCard.addEventListener("click",function(event){
         window.location.href = "/apartment-details";
     });
-
-    var width = document.documentElement.clientWidth;
-    var bodyCard = document.getElementById("bodyCard");
-    var imgCard = document.getElementById("imgCard");
-    if(width < 1000){
-        imgCard.classList.remove("col-md-6");
-        imgCard.classList.add("row-*-*");
-        bodyCard.classList.remove("col-md-6");
-        bodyCard.classList.add("row");       
-    }
-    else{
-        imgCard.classList.add("col-md-6");
-        imgCard.classList.remove("row");
-        bodyCard.classList.add("col-md-6");
-        bodyCard.classList.remove("row");     
-    }
+    window.addEventListener("resize",function(){
+        var width = document.documentElement.clientWidth;
+        var bodyCard = document.getElementById("bodyCard");
+        var imgCard = document.getElementById("imgCard");
+        if(width < 1000){
+            imgCard.classList.remove("col-md-5");
+            imgCard.classList.add("col-md-12");
+            imgCard.classList.remove("card-img-left");
+            imgCard.classList.add("card-img-top");
+            bodyCard.classList.remove("col-md-6");
+            bodyCard.classList.add("row");    
+            console.log("ahhh, im shrinking");   
+        }
+        else{
+            imgCard.classList.remove("col-md-12");
+            imgCard.classList.add("col-md-5");
+            imgCard.classList.remove("card-img-top");
+            imgCard.classList.add("card-img-left");
+            bodyCard.classList.remove("row");     
+            bodyCard.classList.add("col-md-6");
+        }
+    });
+  
 }
 
