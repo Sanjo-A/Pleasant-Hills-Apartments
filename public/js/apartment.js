1102dfb899aab7b544d07655ec241800ca5bb371
Sanjo-A <Sanjo.a@protonmail.com>
2020-02-27 09:23:09 -0600

    alternate card view for apartments

---
+++ public/js/apartment.js
@@ -0,0 +1,6 @@
+window.onload = function(){
+    var listingCard = document.getElementById("listingCard");
+    listingCard.addEventListener("click",function(event){
+        window.location.href = "/apartment-details";
+    });
+}
