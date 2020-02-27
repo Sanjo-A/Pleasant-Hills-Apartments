cb75bee18ca545dc79557c44d5c8aa2ed3d8b4f6
Sanjo-A <Sanjo.a@protonmail.com>
2020-02-27 10:19:25 -0600

    made image slightly smaller

--- public/js/apartment.js
+++ public/js/apartment.js
@@ -9,7 +9,7 @@ window.onload = function(){
         var imgCard = document.getElementById("imgCard");
         if(width < 1000){
             imgCard.classList.remove("col-md-5");
-            imgCard.classList.add("col-md-12");
+            imgCard.classList.add("col-sm-12");
             imgCard.classList.remove("card-img-left");
             imgCard.classList.add("card-img-top");
             bodyCard.classList.remove("col-md-6");
@@ -17,7 +17,7 @@ window.onload = function(){
             console.log("ahhh, im shrinking");   
         }
         else{
-            imgCard.classList.remove("col-md-12");
+            imgCard.classList.remove("col-sm-12");
             imgCard.classList.add("col-md-5");
             imgCard.classList.remove("card-img-top");
             imgCard.classList.add("card-img-left");
