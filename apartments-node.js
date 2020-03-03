663493f7f57b695e68ebc2f05cdd5d6c01520f6c
this-is-my-serious-account <56110943+this-is-my-serious-account@users.noreply.github.com>
2020-03-03 06:46:47 -0500

    Attempt to implement apartment-details (not complete)

--- apartments-node.js
+++ apartments-node.js
@@ -2,13 +2,41 @@ module.exports = function(){
     var express = require('express');
     var router = express.Router();
 
+    // Selects all available apartments
     function getAvailableApartments(res, mysql, context, complete){
-        mysql.pool.query("SELECT aptNumber, numBeds, numBaths, dateAvailable, rent FROM apartments WHERE availabilityStatus = 'Available'", function(error, results, fields){
+        mysql.pool.query("SELECT aptID AS id, aptNumber, numBeds, numBaths, dateAvailable, rent FROM apartments WHERE availabilityStatus = 'Available'", function(error, results, fields){
             if (error){
                 res.write(JSON.stringify(error));
                 res.end();
             }
             context.apartments = results;
+            complete();
+        })
+    }
+
+    // Selects a specific apartment
+    function getApartment(res, mysql, context, id, complete){
+        var sql = "SELECT aptID AS id, aptNumber, numBeds, numBaths, dateAvailable, rent FROM apartments WHERE aptID = ?;";
+        var inserts = [id];
+        mysql.pool.query(sql, inserts, function(error, results, fields){
+            if (error){
+                res.write(JSON.stringify(error));
+                res.end();
+            }
+            context.apartment = results;
+            complete();
+        })
+    }
+
+    // Selects the amenities for a specific apartment
+    function getApartmentAmenities(res, mysql, context, id, complete){
+        var sql = "SELECT aptID AS id, amenID, amenities.amenDescription FROM apartments INNER JOIN apartment_amenities on apartments.aptID = apartment_amenities.aptID INNER JOIN amenities ON apartment_amenities.amenID = amenities.amenID WHERE aptID = ?;";
+        mysql.pool.query(sql, inserts, function(error, results, fields){
+            if (error){
+                res.write(JSON.stringify(error));
+                res.end();
+            }
+            context.amenities = results;
             complete();
         })
     }
@@ -26,6 +54,22 @@ module.exports = function(){
             }
         }
     })
+
+   // Display details on a specific apartment
+    router.get('/:id', function(req, res){
+        callbackCount = 0;
+        var context = {};
+        var mysql = req.app.get('mysql');
+        getApartment(res, mysql, context, req.params.id, complete);
+        getApartmentAmenities(res, mysql, context, req.params.id, complete)
+        function complete(){
+            callbackCount++;
+            if(callbackCount >= 2){
+                res.render('apartment-details', context);
+            }
+
+        }
+    });
 
     return router;
 }();
