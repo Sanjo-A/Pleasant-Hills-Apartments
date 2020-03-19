8efad8228a3b8f73cd242278ebcef323dd634f4e
Sanjo-A <Sanjo.a@protonmail.com>
2020-03-19 03:38:50 -0500

    can add multiple listings after finished adding one to database

--- public/js/manager.js
+++ public/js/manager.js
@@ -74,45 +74,8 @@ $(document).ready(function(){
 });
 $(window).on('load',function(){
     var $allApts = $("#allApts");
-    var newApt = $(
-        '<tr class="newApartment">'
-            +'<form class="createForm">'
-                +'<td class="aptNumber">'
-                    + '<input type="number" name="aptNumber" min = "1" max= "9999" placeholder="Apartment Number">'
-                +'</td>'
-                +'<td class="rent"><input type="number" name="rent" min = "1" max= "9999" placeholder="Rent"></td>'
-                +'<td class="numBeds"><input type="number" name="numBeds" min = "0" max= "10" placeholder="Beds"></td>'
-                +'<td class="numBaths"><input type="number" name="numBaths" min = "0" max= "10" placeholder="Baths"></td>'
-                +'<td class="dateAvailable"><input type="date" name="dateAvailable"></td>'
-                +'<td class="availabilityStatus"><input type="text" name="availabilityStatus" placeholder="Select Availability"></td>'
-                +'<td><button type="submit" class="create" value="save"><img src="/icons/save.png" height="24" width="24"></button></td>'
-            +'</form>'
-        +'</tr>'
-    );
-    $allApts.append(newApt);
-    $(".create").on('click', function(e){
-        e.preventDefault();
-        console.log("trying to create");
-        $.ajax({
-            url:"/api/apartments",
-            type:"PUT",
-            data:{
-                aptNumber: $(this).parents('td').siblings('.aptNumber').find("input").val(),
-                rent:  $(this).parents('td').siblings('.rent').find("input").val(),
-                numBeds:  $(this).parents('td').siblings('.numBeds').find("input").val(),
-                numBaths:  $(this).parents('td').siblings('.numBaths').find("input").val(),
-                dateAvailable:  $(this).parents('td').siblings('.dateAvailable').find("input").val(),
-                availabilityStatus:  $(this).parents('td').siblings('.availabilityStatus').find("input").val()
-            },
-            success: function(i,data){
-                console.log("Added to DB");
-                $(this).show();
-            },
-            error: function(){
-                alert("Could not add to database");
-            }
-        });
-    });
+
+
     $(".newApartment").hide();
 
     $('#allTechs > tr').hover(function(){
@@ -207,7 +170,46 @@ $(window).on('load',function(){
 
     $(".new").on('click', function(){
         $(this).hide();
-        $(".newApartment").show();
+        // $(".newApartment").show();
+        var newApt = $(
+            '<tr class="newApartment">'
+                +'<form class="createForm">'
+                    +'<td class="aptNumber">'
+                        + '<input type="number" name="aptNumber" min = "1" max= "9999" placeholder="Apartment Number">'
+                    +'</td>'
+                    +'<td class="rent"><input type="number" name="rent" min = "1" max= "9999" placeholder="Rent"></td>'
+                    +'<td class="numBeds"><input type="number" name="numBeds" min = "0" max= "10" placeholder="Beds"></td>'
+                    +'<td class="numBaths"><input type="number" name="numBaths" min = "0" max= "10" placeholder="Baths"></td>'
+                    +'<td class="dateAvailable"><input type="date" name="dateAvailable"></td>'
+                    +'<td class="availabilityStatus"><input type="text" name="availabilityStatus" placeholder="Select Availability"></td>'
+                    +'<td><button type="submit" class="create" value="save"><img src="/icons/save.png" height="24" width="24"></button></td>'
+                +'</form>'
+            +'</tr>'
+        );
+        $allApts.append(newApt);
+        $(".create").on('click', function(e){
+            e.preventDefault();
+            console.log("trying to create");
+            $.ajax({
+                url:"/api/apartments",
+                type:"PUT",
+                data:{
+                    aptNumber: $(this).parents('td').siblings('.aptNumber').find("input").val(),
+                    rent:  $(this).parents('td').siblings('.rent').find("input").val(),
+                    numBeds:  $(this).parents('td').siblings('.numBeds').find("input").val(),
+                    numBaths:  $(this).parents('td').siblings('.numBaths').find("input").val(),
+                    dateAvailable:  $(this).parents('td').siblings('.dateAvailable').find("input").val(),
+                    availabilityStatus:  $(this).parents('td').siblings('.availabilityStatus').find("input").val()
+                },
+                success: function(i,data){
+                    console.log("Added to DB");
+                    $(".new").show();
+                },
+                error: function(){
+                    alert("Could not add to database");
+                }
+            });
+        });
     });
 
     //     },
